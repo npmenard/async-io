afd4c6c3e1f488bf8d3681c47e2e1d3fb6ff0c65
Nicolas Menard <nicolas.p.menard@gmail.com>
2025-04-04 12:44:03 -0400

    added print statement

--- src/reactor.rs
+++ src/reactor.rs
@@ -97,14 +97,16 @@ impl Reactor {
 
         REACTOR.get_or_init_blocking(|| {
             crate::driver::init();
-            Reactor {
+            let r = Reactor {
                 poller: Poller::new().expect("cannot initialize I/O event notification"),
                 ticker: AtomicUsize::new(0),
                 sources: Mutex::new(Slab::new()),
                 events: Mutex::new(Events::new()),
                 timers: Mutex::new(BTreeMap::new()),
                 timer_ops: ConcurrentQueue::bounded(TIMER_QUEUE_SIZE),
-            }
+            };
+            log::error!("alignment of REACTOR {}", std::mem::align_of_val(&r));
+            r
         })
     }
 
